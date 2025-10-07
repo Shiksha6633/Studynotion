d66bdaea418e7bbd4bddc80511b2fdb9dde0663a
Shikshasingh <shikshasingh386@gmail.com>
2025-10-07 22:27:23 +0530

    Revert "test"
    This reverts commit 094a26434394f10fc4185a397c1df57e25e55a5e.

--- src/services/apis.js
+++ src/services/apis.js
@@ -6,6 +6,7 @@ export const endpoints = {
   SIGNUP_API: BASE_URL + "/api/v1/auth/signup",
   LOGIN_API: BASE_URL + "/api/v1/auth/login",
   RESETPASSTOKEN_API: BASE_URL + "/api/v1/auth/reset-password-token",
+  RESETPASSWORD_API: BASE_URL + "/api/v1/auth/reset-password",
 }
 
 // PROFILE ENDPOINTS
